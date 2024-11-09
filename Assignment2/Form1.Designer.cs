58eb965cd9e321b44f312420a86b0f0be3d0b142
ArvinderSingh <121319110+Arvinder-Singh-Ghumman@users.noreply.github.com>
2024-11-08 18:55:49 -0700

    Add files via upload

---
+++ Assignment2/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Assignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputData = new System.Windows.Forms.TextBox();
+            this.drawButton = new System.Windows.Forms.Button();
+            this.graph = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputData
+            // 
+            this.inputData.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputData.Location = new System.Drawing.Point(339, 26);
+            this.inputData.Name = "inputData";
+            this.inputData.Size = new System.Drawing.Size(394, 33);
+            this.inputData.TabIndex = 0;
+            this.inputData.TextChanged += new System.EventHandler(this.input_TextChanged);
+            // 
+            // drawButton
+            // 
+            this.drawButton.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.drawButton.ForeColor = System.Drawing.Color.White;
+            this.drawButton.Location = new System.Drawing.Point(590, 65);
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(143, 36);
+            this.drawButton.TabIndex = 1;
+            this.drawButton.Text = "Draw Graph";
+            this.drawButton.UseVisualStyleBackColor = false;
+            this.drawButton.Click += new System.EventHandler(this.drawButton_Click);
+            // 
+            // graph
+            // 
+            this.graph.Location = new System.Drawing.Point(30, 122);
+            this.graph.Name = "graph";
+            this.graph.Size = new System.Drawing.Size(758, 288);
+            this.graph.TabIndex = 2;
+            this.graph.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Data seperated by commas -";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.graph);
+            this.Controls.Add(this.drawButton);
+            this.Controls.Add(this.inputData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.graph)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputData;
+        private System.Windows.Forms.Button drawButton;
+        private System.Windows.Forms.PictureBox graph;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Assignment2/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment2
+{
+    public partial class Form1 : Form
+    {
+        private List<float> section = new List<float>();
+        private List<Color> secColor = new List<Color>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            graph.Paint += graphPaint;
+        }
+
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void DrawGraph()
+        {
+            try
+            {
+                string input = inputData.Text;
+
+                var numbers = input.Split(',')
+                                   .Select(num => num.Trim())
+                                   .Where(num => !string.IsNullOrEmpty(num))
+                                   .Select(num => Convert.ToDouble(num))
+                                   .ToList();
+
+                if (numbers.Count == 0)
+                {
+                    MessageBox.Show("Please enter valid numbers separated by commas.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                //reset
+                double total = numbers.Sum();
+                float startAngle = 0f;
+                section.Clear();
+                secColor.Clear();
+
+                //getting and sotring angle and color for each
+                Random rand = new Random();
+                foreach (var number in numbers)
+                {
+                    double percentage = number / total;
+                    float angle = (float)(percentage * 360);
+
+                    section.Add(angle);
+                    secColor.Add(Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256)));
+
+                    startAngle += angle;
+                }
+
+                // repaint
+                graph.Invalidate();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter numbers separated by commas.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void graphPaint(object sender, PaintEventArgs e)
+        {
+            //no data
+            if (section.Count == 0) return; 
+
+            Graphics g = e.Graphics;
+            float startAngle = 0f;
+
+            //legend pos
+            int legendX = graph.Width - 100; 
+            int legendY = 10; 
+
+            for (int i = 0; i < section.Count; i++)
+            {
+                // Draw section
+                using (Brush brush = new SolidBrush(secColor[i]))
+                {
+                    g.FillPie(brush, 10, 10, graph.Width - 120, graph.Height - 20, startAngle, section[i]);
+                }
+                startAngle += section[i];
+
+                // Draw the legend
+                using (Brush brush = new SolidBrush(secColor[i]))
+                {
+                    g.FillRectangle(brush, legendX, legendY, 15, 15);
+                }
+                string labelText = $"Slice {i + 1} ({Math.Round(section[i] / 360 * 100, 2)}%)";
+                using (Brush textBrush = new SolidBrush(Color.Black))
+                {
+                    g.DrawString(labelText, this.Font, textBrush, legendX + 20, legendY);
+                }
+                legendY += 25;
+            }
+        }
+
+
+        private void drawButton_Click(object sender, EventArgs e)
+        {
+            DrawGraph();
+        }
+    }
+
+    }
+
+
